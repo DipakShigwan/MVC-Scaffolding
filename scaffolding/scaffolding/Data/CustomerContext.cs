94d58833dc976ed8f40cd931335060393598dabb
Dipak Shigwan <shigwandipak123@gmail.com>
2022-10-19 21:48:35 +0530

    MVC model to controller Scaffolding
    just a demo of
    manually model created and scaffolded as controller using Entity framework template

---
+++ scaffolding/scaffolding/Data/CustomerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using scaffolding.Models;
+
+namespace scaffolding.Data
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext (DbContextOptions<CustomerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<scaffolding.Models.Customer> Customer { get; set; } = default!;
+    }
+}
---
+++ scaffolding/scaffolding/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace scaffolding.Models
+{
+    public class Customer
+    {
+        [Key]
+        public string? CustName { get; set; }
+        public string? Address { get; set; }
+
+
+    }
+}
